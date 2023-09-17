abb06e12597299ac53283962b3433c809baedf1a
Anass A.Thani <58435912+anass-at@users.noreply.github.com>
2023-09-18 01:23:29 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 Handlebars.registerPartial('header',`
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background: #25274D !important;">
 <div class="container">
-  <a class="navbar-brand d-flex" href="/" >
+  <a class="navbar-brand d-flex" href="index.html" >
   <i class="bi bi-newspaper display-5 me-2"></i>
     
     <div class="ms-2 me-2"> 
@@ -16,7 +16,7 @@ Handlebars.registerPartial('header',`
   <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center w-100">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="all-news.html">News</a>
